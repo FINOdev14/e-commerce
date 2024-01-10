5352bc3fab9f42b31e0e1f32cbf64428cf7c3d62
dev13 <dev13@codecraftdev.com>
2024-01-10 12:55:07 -0500

    feat: Se pone a correr el servidor

--- src/app.js
+++ src/app.js
@@ -28,8 +28,9 @@ const resolvers = require('./merge/mergeResolver')
 
 const PORT = process.env.PORT || 5020
 
+//configuración del servidor de apollo y la conexion con el servidor de express
 async function start(){
-  const schema = makeExecutableSchema({typeDefs, resolvers})
+  const schema = makeExecutableSchema({ typeDefs, resolvers })
   const apolloServer = new ApolloServer({
     schema,
     plugins: [
@@ -38,8 +39,8 @@ async function start(){
       })
     ]
   })
-  await apolloServer.start()
-  apolloServer.applyMiddleware({app})
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
 
   app.listen(PORT, () => {
     console.log(`e-commerce ready at port: http://localhost:${PORT}/graphql`);
@@ -47,6 +48,7 @@ async function start(){
   })
 }
 
+//iniciando servidor
 start()
 
 
